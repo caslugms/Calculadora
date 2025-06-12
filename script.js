3d9e2c2289f8b9d45cf423c412b8640c7659f800
Lucas <lukinhasx248@gmail.com>
2025-06-12 19:55:55 -0300

    terminei

--- script.js
+++ script.js
@@ -10,6 +10,21 @@ function appendOperator(operator) {
     document.getElementById('screen').value = currentInput;
 }
 
+function appendParenthesis(parenthesis) {
+    currentInput += parenthesis;
+    document.getElementById('screen').value = currentInput;
+}
+
+function appendDecimal() {
+    const parts = currentInput.split(' ');
+    const lastPart = parts[parts.length - 1];
+    
+    if (!lastPart.includes('.')) {
+        currentInput += '.';
+        document.getElementById('screen').value = currentInput;
+    }
+}
+
 function clearScreen() {
     currentInput = '';
     document.getElementById('screen').value = '';
@@ -21,6 +36,9 @@ function calculateResult() {
         document.getElementById('screen').value = currentInput;
     } catch (e) {
         document.getElementById('screen').value = 'Erro';
-        currentInput = '';
+        setTimeout(() => {
+            currentInput = '';
+            document.getElementById('screen').value = '';
+        }, 1500);
     }
 }
